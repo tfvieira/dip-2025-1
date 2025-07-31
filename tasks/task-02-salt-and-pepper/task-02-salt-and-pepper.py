2aaa69ec42ddd161d1e9a28a82ab1d20eb4112eb
{nivuciis} <vinicius.rafael751@gmail.com>
2025-07-31 14:14:58 -0300

    feat: Add salt and pepper noise to image task 02 for DIP 2025-1

--- tasks/task-02-salt-and-pepper/task-02-salt-and-pepper.py
+++ tasks/task-02-salt-and-pepper/task-02-salt-and-pepper.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 def create_salt_and_pepper_noise(height=100, width=100, salt_prob=0.05, pepper_prob=0.05):
     """
     Returns an image ∈ [-1, 1] containing salt (I = 1.0) and 
@@ -8,6 +7,20 @@ def create_salt_and_pepper_noise(height=100, width=100, salt_prob=0.05, pepper_p
     """
     ### START CODE HERE ###
     # TODO
+    #img = np.random.choice([0.5, -1.0, 1.0], 
+     #                      p=[1 - salt_prob - pepper_prob, 
+      #                       pepper_prob, salt_prob],
+       #                     size=(height, width))
+    img = np.random.rand(height, width)
+    for i in range(height):
+        for j in range(width):
+            if img[i, j] < salt_prob:
+                img[i, j] = 1.0
+            elif img[i, j] < salt_prob + pepper_prob:
+                img[i, j] = -1.0
+            else:
+                img[i, j] = 0.5
+    
     ### END CODE HERE ###
     return img
 
